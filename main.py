8dfd83cecaa0fb3de60c702ccdd7e10649fe7139
DarecekParecek <D.t.tunega@post.cz>
2023-05-17 22:21:13 +0200

    readability

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ def click(x, y):
     time.sleep(0.2)
 
 
-def detect_screen_type(screenshot, templates):
-    screenshot_gray = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2GRAY)
+def detectScreenType(screenshot, templates):
+    screenshotGray = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2GRAY)
     threshold = 0.6
-    matching_screens = []
+    matchingScreens = []
 
-    for template_path, screen_type in templates.items():
-        template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
-        template_w, template_h = template.shape[::-1]
-        result = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv2.minMaxLoc(result)
-        if max_val >= threshold:
-            matching_screens.append(screen_type)
+    for templatePath, screenType in templates.items():
+        template = cv2.imread(templatePath, cv2.IMREAD_GRAYSCALE)
+        templateW, templateH = template.shape[::-1]
+        result = cv2.matchTemplate(screenshotGray, template, cv2.TM_CCOEFF_NORMED)
+        _, maxVal, _, maxLoc = cv2.minMaxLoc(result)
+        if maxVal >= threshold:
+            matchingScreens.append(screenType)
 
-    return matching_screens
+    return matchingScreens
 
 
 def searchForButton(buttonPath, threshold=0.6, minX=0, maxX=0, minY=0, maxY=0):
@@ -58,54 +58,49 @@ def clickOnButtonWithOffset(buttonPath, threshold=0.6, minX=0, maxX=0, minY=0, m
 
 
 templates = {
-    'gameNavigationReferences/MainMenu.png': 'main_menu',
-    'gameNavigationReferences/ChooseYourDeck.png': 'deck_selection',
-    'gameNavigationReferences/FindingOpponent.png': 'gameSearch',
-    'gameNavigationReferences/VS.png': 'card_selection',
-    'gameNavigationReferences/EnemyTurn.png': 'enemy_turn',
-    'gameNavigationReferences/EndTurn.png': 'my_turn',
-    'gameNavigationReferences/EndOfGame.png': 'end_game',
-    'gameNavigationReferences/gameIssue.png': 'game_issue',
-    'gameNavigationReferences/DesktopPlay.png': 'play_button_client',
-    'gameNavigationReferences/DesktopBlizzard.png': 'blizzard_logo',
-    'gameNavigationReferences/wildModeSelected.png': 'wild_mode_selected',
-    'gameNavigationReferences/casualModeSelected.png': 'casual_mode_selected',
-    'gameNavigationReferences/casualModeSelect.png': 'mode_select',
-    'Buttons/PlayButton.png': 'gameplay_button'
+    "gameNavigationReferences/MainMenu.png": "mainMenu",
+    "gameNavigationReferences/ChooseYourDeck.png": "deckSelection",
+    "gameNavigationReferences/FindingOpponent.png": "gameSearch",
+    "gameNavigationReferences/VS.png": "cardSelection",
+    "gameNavigationReferences/EnemyTurn.png": "enemyTurn",
+    "gameNavigationReferences/EndTurn.png": "myTurn",
+    "gameNavigationReferences/EndOfGame.png": "endGame",
+    "gameNavigationReferences/gameIssue.png": "gameIssue",
+    "gameNavigationReferences/DesktopPlay.png": "playButtonClient",
+    "gameNavigationReferences/DesktopBlizzard.png": "blizzardLogo",
+    "gameNavigationReferences/wildModeSelected.png": "wildModeSelected",
+    "gameNavigationReferences/casualModeSelected.png": "casualModeSelected",
+    "gameNavigationReferences/casualModeSelect.png": "modeSelect",
+    "Buttons/PlayButton.png": "gameplayButton"
 }
 
 screenshot = pyautogui.screenshot()
-current_screen = detect_screen_type(screenshot, templates)
+currentScreen = detectScreenType(screenshot, templates)
 
 
 while True:
     time.sleep(0.8)
     screenshot = pyautogui.screenshot()
-    current_screen = detect_screen_type(screenshot, templates)
-    print(current_screen)
+    currentScreen = detectScreenType(screenshot, templates)
+    print(currentScreen)
 
-    if 'main_menu' in current_screen:
-        clickOnButtonWithOffset('gameNavigationReferences/MainMenu.png', threshold=0.4, minX=50, maxX=90, minY=5, maxY=51)
+    if "mainMenu" in currentScreen:
+        clickOnButtonWithOffset("gameNavigationReferences/MainMenu.png", threshold=0.4, minX=50, maxX=90, minY=5, maxY=51)
 
-    if 'wild_mode_selected' in current_screen:
-        click(721,506)
+    if "wildModeSelected" in currentScreen:
+        click(721, 506)
         time.sleep(0.5)
-        print("change game mode")
+        print("changeGameMode")
         time.sleep(0.3)
-        clickOnButtonWithOffset('gameNavigationReferences/wildModeSelected.png', threshold=0.6, minX=50, maxX=90, minY=5, maxY=51)
-    if 'mode_select' in current_screen:
-        clickOnButtonWithOffset('gameNavigationReferences/casualModeSelect.png', threshold=0.6, minX=150, maxX=320,minY=50, maxY=220)
-        print("clicking on casual")
+        clickOnButtonWithOffset("gameNavigationReferences/wildModeSelected.png", threshold=0.6, minX=50, maxX=90, minY=5, maxY=51)
+    if "modeSelect" in currentScreen:
+        clickOnButtonWithOffset("gameNavigationReferences/casualModeSelect.png", threshold=0.6, minX=150, maxX=320, minY=50, maxY=220)
+        print("clickingOnCasual")
         time.sleep(0.3)
-    if 'casual_mode_selected' and 'gameplay_button' in current_screen:
-        clickOnButtonWithOffset('Buttons/PlayButton.png', threshold=0.6, minX=150, maxX=180,minY=50, maxY=220)
-        print("clicking on play button")
+    if "casualModeSelected" and "gameplayButton" in currentScreen:
+        clickOnButtonWithOffset("Buttons/PlayButton.png", threshold=0.6, minX=150, maxX=180, minY=50, maxY=220)
+        print("clickingOnPlayButton")
         time.sleep(0.3)
-    if 'gameSearch' in current_screen:
+    if "gameSearch" in currentScreen:
         time.sleep(0.2)
-        print("finding a game")
-
-
-
-
-
+        print("findingAGame")
